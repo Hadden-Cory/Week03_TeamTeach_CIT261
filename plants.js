e76a2be62183f9b57c58c8fdc7e40d7bb3baaddf
Mona Lisa <had17007@byui.edu>
2019-01-23 15:04:53 -0700

    Adding initial files

---
+++ plants.js
@@ -0,0 +1,13 @@
+function makeRed(){
+  document.getElementById("one").style.backgroundColor = "red";
+  setTimeout(function(){document.getElementById("one").style.backgroundColor = "rgb(205, 245, 253)";}, 2000); 
+}
+
+function crazyToggle(){
+    document.getElementById("two").classList.toggle("crazy"); 
+  }
+
+  function invisibleToggle(){
+    document.getElementById("three").classList.toggle("invisible");
+    setTimeout(function(){document.getElementById("three").classList.toggle("invisible")}, 2000); 
+  }
