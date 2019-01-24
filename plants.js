ceac01ead25c16c6a98ba46a8bd7a454bb86e116
Mona Lisa <had17007@byui.edu>
2019-01-23 21:58:36 -0700

    What we did over the meeting

--- plants.js
+++ plants.js
@@ -1,13 +1,22 @@
-function makeRed(){
+function makeRed() {
   document.getElementById("one").style.backgroundColor = "red";
-  setTimeout(function(){document.getElementById("one").style.backgroundColor = "rgb(205, 245, 253)";}, 2000); 
+  setTimeout(function () {
+    document.getElementById("one").style.backgroundColor = "rgb(205, 245, 253)";
+  }, 2000);
 }
 
-function crazyToggle(){
-    document.getElementById("two").classList.toggle("crazy"); 
-  }
+function crazyToggle() {
+  document.getElementById("two").classList.toggle("crazy");
+}
 
-  function invisibleToggle(){
-    document.getElementById("three").classList.toggle("invisible");
-    setTimeout(function(){document.getElementById("three").classList.toggle("invisible")}, 2000); 
-  }
+function invisibleToggle() {
+  document.getElementById("three").classList.toggle("invisible");
+  setTimeout(function () {
+    document.getElementById("three").classList.toggle("invisible")
+  }, 2000);
+}
+
+function blowUp() {
+  document.getElementById("four").classList.add("blowUp");
+  setTimeout(function(){document.getElementById("four").classList.remove("blowUp");}, 5000);
+}
